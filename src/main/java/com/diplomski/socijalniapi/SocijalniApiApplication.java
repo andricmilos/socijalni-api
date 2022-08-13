f9851daaad298d69e26ef2e75a89500bf114c2c4
Milos <milosandric96@gmail.com>
2022-08-13 15:11:19 +0200

    Initial commit

---
+++ src/main/java/com/diplomski/socijalniapi/SocijalniApiApplication.java
@@ -0,0 +1,13 @@
+package com.diplomski.socijalniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocijalniApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocijalniApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomski/socijalniapi/SocijalniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomski.socijalniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocijalniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
