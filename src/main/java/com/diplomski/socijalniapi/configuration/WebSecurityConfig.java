87e27be9cb01628dc73b018997a339cc5fd083ea
Milos Andric <milosandric2000@gmail.com>
2023-01-21 09:24:55 +0100

    Cors dozvola za klijentski deo

--- src/main/java/com/diplomski/socijalniapi/configuration/WebSecurityConfig.java
+++ src/main/java/com/diplomski/socijalniapi/configuration/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
