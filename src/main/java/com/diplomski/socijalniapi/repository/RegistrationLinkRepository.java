6ab8670a86f1a7ea503df2aa8cccf2da20175101
Milos Andric <milosandric2000@gmail.com>
2023-02-02 14:23:40 +0100

    Brisanje nepotrebnih/neiskoriscenih klasa

--- src/main/java/com/diplomski/socijalniapi/repository/RegistrationLinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplomski.socijalniapi.repository;
-
-import com.diplomski.socijalniapi.entity.RegistrationLink;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationLinkRepository extends JpaRepository<RegistrationLink,Integer> {
-}
